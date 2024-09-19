22914ce5427398ca3b13bdeb66c3cb00da7db72d
Ermesson Andrade Belarmino <20231011110017@ifrn.local>
2024-09-19 08:25:11 -0300

    favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 import Header from './components/Header.js';
 
+function loadFavicon() {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = 'image/png';
+    link.href = '/assets/images/pngs/favicon.png';
+    document.head.appendChild(link);
+}
+
 function loadHeader() {
     const header = new Header();
     header.createHeader();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    loadFavicon();
     loadHeader();
 });
