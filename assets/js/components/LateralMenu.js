d92bfa2ef5fbf34b5de3d810ae6cbde366036524
Ermesson Andrade Belarmino <20231011110017@ifrn.local>
2024-09-20 17:41:09 -0300

    posts updates

--- assets/js/components/LateralMenu.js
+++ assets/js/components/LateralMenu.js
@@ -16,31 +16,31 @@ export default class LateralMenu {
                     <li class="thin-border-bottom">
                         <a href="index.html">
                             <img src="${homeIcon}" alt="Home">
-                            <span class="nova-square-font">Home</span>
+                            <span class="nova-square-font home">Home</span>
                         </a>
                     </li>
                     <li class="thin-border-bottom">    
                         <a href="messages.html">
                             <img src="${messagesIcon}" alt="Messages">
-                            <span class="nova-square-font">Mensagens</span>
+                            <span class="nova-square-font messages">Mensagens</span>
                         </a>
                     </li>
                     <li class="thin-border-bottom">
                         <a href="profile.html">
                             <img src="${profileIcon}" alt="Profile">
-                            <span class="nova-square-font">Perfil</span>
+                            <span class="nova-square-font profile">Perfil</span>
                         </a>
                     </li>
                     <li class="thin-border-bottom">
                         <a href="post.html">
                             <img src="${postIcon}" alt="Post">
-                            <span class="nova-square-font">Postagem</span>
+                            <span class="nova-square-font post">Postagem</span>
                         </a>
                     </li>
                     <li class="thin-border-bottom">
                         <a href="settings.html">
                             <img src="${settingsIcon}" alt="Settings">
-                            <span class="nova-square-font">Configurações</span>
+                            <span class="nova-square-font settings">Configurações</span>
                         </a>
                     </li>
                 </ul>
